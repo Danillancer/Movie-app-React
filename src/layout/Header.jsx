7ef29cf74d12f2211839a0631392e20611371c7c
Данил Голубов <danilrip065r@gmail.com>
2022-07-30 21:54:32 -0400

    movie-app

---
+++ src/layout/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return <nav className="blue lighten-1">
+        <div className="nav-wrapper">
+        <a href="#" className="brand-logo">React Movies</a>
+        
+        </div>
+    </nav>
+}
+
+export {Header}
